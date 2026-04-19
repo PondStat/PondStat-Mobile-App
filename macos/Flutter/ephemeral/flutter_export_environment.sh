6b203a7dd775e6509b99a32a5123e8bac0f2b7b6
renzoupv <rpdearroz@up.edu.ph>
2026-04-19 19:04:58 +0800

    refactor: rebase with main files

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/renzooo/Documents/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/renzooo/Documents/School/3rd Yr - 2nd Sem/CMSC 156/PondStat-Mobile-App"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
